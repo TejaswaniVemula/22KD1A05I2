c6d6916aba9f7ee14efceda8a7cc3bc7291061f9
TejaswaniVemula <tejaswanivemula73@gmail.com>
2025-08-19 11:28:28 +0530

    header update

---
+++ Frontend_URLShortener/src/Header.jsx
@@ -0,0 +1,10 @@
+
+function Header() {
+  return (
+    <header className="App-header">
+      <h1>URL Shortener</h1>
+      <p>Shorten your URLs easily!</p>
+    </header>
+  );
+}
+export default Header;
