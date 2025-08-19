26b0a741bd97b9ff59fb9b38768d95362409a5ad
TejaswaniVemula <tejaswanivemula73@gmail.com>
2025-08-19 12:06:10 +0530

    add url input

--- Frontend_URLShortener/src/App.jsx
+++ Frontend_URLShortener/src/App.jsx
@@ -1,17 +1,22 @@
 import './App.css'
 import Header from './Header.jsx'
 import Shortener from './Shortener.jsx'
+import Button from './Button.jsx'
 function App() {
   
 
   return (
     <>
-      <Header/> 
+      <div className="App">
+       <Header/> 
       <Shortener/>
       <Shortener/>
       <Shortener/>
       <Shortener/>
       <Shortener/>
+      <Button/>
+      </div>
+      
     </>
   )
 }
---
+++ Frontend_URLShortener/src/Button.jsx
@@ -0,0 +1,8 @@
+function Button() {
+  return (
+    <button className="custom-button" >
+        Shorten URL
+    </button>
+  );
+}
+export default Button;
